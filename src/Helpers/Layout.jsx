3d053d11f81bf995d954a9af7389552cd9e16a0a
Mukles <Mukleshossen1763@gmail.com>
2022-06-12 18:13:45 +0600

    done

--- src/Helpers/Layout.jsx
+++ src/Helpers/Layout.jsx
@@ -1,9 +1,14 @@
+import React, { useEffect } from "react";
+
 const Layout = ({ children }) => {
+  useEffect(() => {}, []);
   return (
-    <div className="w-full px-5 py-10 ml-auto shadow-sm lg:w-[calc(100%-304px)]">
-      {children}
-    </div>
+    <>
+      <div className="w-full px-5 py-10 ml-auto shadow-sm lg:w-[calc(100%-304px)]">
+        {children}
+      </div>
+    </>
   );
 };
 
-export default Layout;
+export default React.memo(Layout);
